d9d460fdb0976c50669d82ae1d54a4e8e620405c
ranagoharayoub <ranagohar1163@gmail.com>
2021-08-03 20:52:09 +0500

    DepositCalculation

--- src/Screens/DepositFunds.js
+++ src/Screens/DepositFunds.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import './DepositFunds.css'
 
 function DepositFunds() {
     const history = useHistory()
+    const [amount, setamount] = useState('')
+    const [fee, setfee] = useState(null)
+    const [total, settotal] = useState(null)
+
+    useEffect(() => {
+       setfee(parseFloat((Number(amount)*0.029)+0.30).toFixed(2))
+       console.log(fee, typeof(fee), fee+amount)
+       settotal(Number(fee)+Number(amount))
+    }, [amount, fee, total])
 
     const redirect = (e)=>{
         e.preventDefault()
@@ -26,7 +35,14 @@ function DepositFunds() {
                 </div>
                 <form className='form' onSubmit={redirect}>
                     <label htmlFor='amonut' className='label'>Deposit Amount</label>
-                    <input required className='input' placeholder='Deposit Amount' type='number'></input>
+                    <input required
+                        value={amount}
+                        onChange={(e)=> setamount(e.target.value)} 
+                        className='input' 
+                        placeholder='Deposit Amount' 
+                        type='number'
+                        >
+                    </input>
                     <div className='minimun-input'>$5.00 minimum</div>
                     <div className='instr'>You can deposit funds to shop and enter cash matches. All funds earned through tournament play can be withdrawn.</div>
                     <div className='disc-amount'>
@@ -34,7 +50,7 @@ function DepositFunds() {
                             Deposit Amount
                         </div>
                         <div className='amount'>
-                            $57.00
+                            ${amount? amount: '0'}
                         </div>
                     </div>
                     <div className='disc-amount'>
@@ -42,7 +58,7 @@ function DepositFunds() {
                             Fees (2.9% + 0.30)
                         </div>
                         <div className='amount'>
-                            $3.72
+                            ${ amount? fee: '0'}
                         </div>
                     </div>
                     <hr></hr>
@@ -51,7 +67,7 @@ function DepositFunds() {
                             Total
                         </div>
                         <div className='amount'>
-                            $60.72
+                            ${amount? total: '0'}
                         </div>
                     </div>
                     <div className='disc-amount'>
